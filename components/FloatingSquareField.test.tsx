d688ade2b547c6ccee275c5bc99d65436a2bf8ab
Emin Emre <emin3619@gmail.com>
2026-07-12 09:57:56 +0300

    chore: clean hero animation test imports

--- components/FloatingSquareField.test.tsx
+++ components/FloatingSquareField.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { useReducedMotion } from 'framer-motion';
 import { FloatingSquareField } from './FloatingSquareField';
